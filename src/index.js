eb11d2c6e537c2d157eb8312302fc5df99e39af6
Bhuwan Tiwari <bhuwan1200@gmail.com>
2024-02-05 12:27:47 +0530

    setup of smpt using noe mailer done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const{PORT}=require('./config/serverConfig')
+const { PORT } = require('./config/serverConfig')
+const {sendBasicEmail}= require('./services/email-service')
+const setupAndStartServer = () => {
+    const app = express()
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
 
-const setupAndStartServer =()=>{
-const app = express()
-app.use(bodyParser.json()) 
-app.use(bodyParser.urlencoded({extended : true}))
-
-app.listen(PORT,async()=>
-{
-    console.log(`server started at poer ${PORT}`)
-
-})
+    app.listen(PORT, async () => {
+        console.log(`server started at poer ${PORT}`)
+        sendBasicEmail(
+            'test@gmail.com',
+            'bhuwan.21ug1088@iiitranchi.ac.in',
+            "remainder service",
+            "hope this remainder service message reached to you"
+        )
+    })
 
 }
 setupAndStartServer();
